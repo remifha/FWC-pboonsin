82944ee3a867a6084637186035ad70c5752fb850
remifha <pariyafahii@gmail.com>
2026-09-19 03:30:09 +0700

    edit and add file

---
+++ discovery-piscine/cell03/ex00/background.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("colorBTN");
+
+btn.addEventListener("click",() =>{
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+})
